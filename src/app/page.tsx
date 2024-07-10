870a744945d1e5a16b37f18b1c355c52524c42d4
muzamilbaig123 <muzamilbaig24@gmail.com>
2024-07-10 15:53:42 +0500

    micro change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,8 @@ import Image from "next/image";
 */
 
 
-
+// npm latest version insatll
+// npm install -g npm@10.8.1
 
 
 
@@ -133,8 +134,10 @@ export default function Home () {
       <h1>Parent H1</h1>
       <h2>Parent H2</h2>
       <div className="innerasa">
-        <img src="https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="picter 1" />
+        <img src="https://images.unsplash.com/photo-1613323593608-abc90fec84ff?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="40%" alt="picter 1" />
       </div>
     </div>
   )
 }
+
+
